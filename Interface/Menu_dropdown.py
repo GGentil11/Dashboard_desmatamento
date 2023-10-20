6281e06881ebe26a32b6a53b141ee599ff589365
Gabriel Felipe Baia Gentil <97031050+GGentil11@users.noreply.github.com>
2023-10-20 10:28:41 -0300

    Update Menu_dropdown.py

--- Interface/Menu_dropdown.py
+++ Interface/Menu_dropdown.py
@@ -4,8 +4,8 @@ from Graficos.Por_ano import por_ano
 from Graficos.Por_estado import por_estado
 from Graficos.Por_fenomeno import por_fenomeno
 
-df = pd.read_csv('Datasets\Desmatamento.csv', sep=',')
-df_fenomeno = pd.read_csv('Datasets\el_nino_la_nina_1999_2019.csv')
+df = pd.read_csv('Datasets/Desmatamento.csv', sep=',')
+df_fenomeno = pd.read_csv('Datasets/el_nino_la_nina_1999_2019.csv')
 def menu_radio():
     st.sidebar.caption(f'Dados retirados do Dataset: [Brazilian Amazon Rainforest Degradation 1999-2019](https://www.kaggle.com/datasets/mbogernetto/brazilian-amazon-rainforest-degradation)')
     tipo_pesquisa = st.sidebar.radio('Selecione o filtro', ['Por ano', 'Por estado', 'Influência de Fenômenos Climáticos'], 
